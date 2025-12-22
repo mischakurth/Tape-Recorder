e757913bd616dc874ad2b36150a7b099a6673d9b
mischakurth <github@mischakurth.de>
2025-12-22 02:31:41 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -26,19 +26,16 @@ class AudioUNet(nn.Module):
         super(AudioUNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-
-        # Durch 'base_channels' können wir das Modell skalieren
         bc = base_channels
 
-        # Encoder (Downsampling)
-        # 32 -> 64 -> 128 -> 256 -> 512 (statt bis 1024)
+        # Encoder
         self.inc = DoubleConv(n_channels, bc)
         self.down1 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(bc, bc * 2))
         self.down2 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(bc * 2, bc * 4))
         self.down3 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(bc * 4, bc * 8))
-        self.down4 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(bc * 8, bc * 16))  # Bottleneck (jetzt 512)
+        self.down4 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(bc * 8, bc * 16))
 
-        # Decoder (Upsampling)
+        # Decoder
         self.up1 = nn.ConvTranspose2d(bc * 16, bc * 8, kernel_size=2, stride=2)
         self.conv1 = DoubleConv(bc * 16, bc * 8)
 
@@ -54,6 +51,20 @@ class AudioUNet(nn.Module):
         # Output Layer
         self.outc = nn.Conv2d(bc, n_classes, kernel_size=1)
 
+    def _pad_and_cat(self, x_up, x_skip):
+        """
+        Hilfsfunktion: Passt x_up an die Größe von x_skip an (durch Padding)
+        und verkettet sie dann. Verhindert Crashes bei ungeraden Dimensionen.
+        """
+        diffY = x_skip.size()[2] - x_up.size()[2]
+        diffX = x_skip.size()[3] - x_up.size()[3]
+
+        # Padding: (Left, Right, Top, Bottom)
+        x_up = F.pad(x_up, [diffX // 2, diffX - diffX // 2,
+                            diffY // 2, diffY - diffY // 2])
+
+        return torch.cat([x_skip, x_up], dim=1)
+
     def forward(self, x):
         x1 = self.inc(x)
         x2 = self.down1(x1)
@@ -61,25 +72,21 @@ class AudioUNet(nn.Module):
         x4 = self.down3(x3)
         x5 = self.down4(x4)
 
+        # Upsampling Path mit sicherer Concatenation
         x = self.up1(x5)
-        # Padding Logik für ungerade Dimensionen
-        diffY = x4.size()[2] - x.size()[2]
-        diffX = x4.size()[3] - x.size()[3]
-        x = F.pad(x, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
-
-        x = torch.cat([x4, x], dim=1)
+        x = self._pad_and_cat(x, x4)  # <--- Safe Cat
         x = self.conv1(x)
 
         x = self.up2(x)
-        x = torch.cat([x3, x], dim=1)
+        x = self._pad_and_cat(x, x3)  # <--- Safe Cat
         x = self.conv2(x)
 
         x = self.up3(x)
-        x = torch.cat([x2, x], dim=1)
+        x = self._pad_and_cat(x, x2)  # <--- Safe Cat
         x = self.conv3(x)
 
         x = self.up4(x)
-        x = torch.cat([x1, x], dim=1)
+        x = self._pad_and_cat(x, x1)  # <--- Safe Cat
         x = self.conv4(x)
 
         return self.outc(x)
